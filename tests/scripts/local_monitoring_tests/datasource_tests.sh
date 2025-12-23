4835decc57f24d83de7a20e12e8f8fd90829702c
Saad Khan <saakhan@ibm.com>
2025-12-23 10:29:58 +0530

    add startup tests for datasource validations
    Signed-off-by: Saad Khan <saakhan@ibm.com>

--- tests/scripts/local_monitoring_tests/datasource_tests.sh
+++ tests/scripts/local_monitoring_tests/datasource_tests.sh
@@ -16,10 +16,16 @@
 #
 #
 
+<<<<<<< HEAD
 # Get the absolute path of current directory
 LOCAL_MONITORING_TEST_DIR="${KRUIZE_REPO}/tests/scripts/local_monitoring_tests"
 
 # Source the common functions scripts
+=======
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
+LOCAL_MONITORING_TEST_DIR="${CURRENT_DIR}/local_monitoring_tests"
+
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 . ${LOCAL_MONITORING_TEST_DIR}/../common/common_functions.sh
 
 APP_DEPLOYMENT="kruize"
@@ -27,6 +33,7 @@ APP_DEPLOYMENT="kruize"
 # Datasource serviceName overrides to simulate reachability
 declare -A datasource_scenarios
 datasource_scenarios=(
+<<<<<<< HEAD
   ["both-invalid"]="invalid invalid"
   ["both-valid"]="prometheus-k8s thanos-querier"
   ["prom-valid-thanos-invalid"]="prometheus-k8s invalid-thanos"
@@ -40,6 +47,15 @@ datasource_scenario_order=(
 )
 
 function datasource_tests() {
+=======
+  ["both-valid"]="prometheus-k8s thanos-querier"
+  ["prom-valid-thanos-invalid"]="prometheus-k8s invalid-thanos"
+  ["prom-invalid-thanos-valid"]="invalid-prometheus thanos-querier"
+  ["both-invalid"]="invalid-prometheus invalid-thanos"
+)
+
+function datasource_reachability_tests() {
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	start_time=$(get_date)
 	FAILED_CASES=()
 	TESTS=0
@@ -47,12 +63,25 @@ function datasource_tests() {
 	TESTS_PASSED=0
 	((TOTAL_TEST_SUITES++))
 
+<<<<<<< HEAD
 	TEST_SUITE_DIR="${RESULTS}/datasource_tests"
 	mkdir -p "${TEST_SUITE_DIR}" 2>&1
 	KRUIZE_SETUP_LOG="${TEST_SUITE_DIR}/kruize_setup.log"
 	KRUIZE_POD_LOG="${TEST_SUITE_DIR}/kruize_pod.log"
   target="crc"
 	echo ""
+=======
+	TEST_SUITE_DIR="${RESULTS}/datasource_reachability_tests"
+	mkdir -p "${TEST_SUITE_DIR}" 2>&1
+	KRUIZE_SETUP_LOG="${TEST_SUITE_DIR}/kruize_setup.log"
+
+	echo ""
+	echo "Setting up kruize..." | tee -a ${LOG}
+	setup >> "${KRUIZE_SETUP_LOG}" 2>&1
+	echo "Setting up kruize...Done" | tee -a ${LOG}
+	sleep 15
+
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	if [ "$cluster_type" == "minikube" ] || [ "$cluster_type" == "kind" ]; then
 		NAMESPACE="monitoring"
 		YAML_FILE="${LOCAL_MONITORING_TEST_DIR}/../../../manifests/crc/default-db-included-installation/minikube/kruize-crc-minikube.yaml"
@@ -63,6 +92,7 @@ function datasource_tests() {
 		echo "Invalid cluster type found: ${cluster_type}"
 		return
 	fi
+<<<<<<< HEAD
 	cleanup_datasources_from_yaml
 	echo "Setting up kruize..." | tee -a ${LOG}
 	echo "${KRUIZE_SETUP_LOG}"
@@ -73,6 +103,8 @@ function datasource_tests() {
 	popd > /dev/null
 	# restore the yaml once the setup is done
 	mv "${YAML_FILE}.pre_setup.bak" "$YAML_FILE"
+=======
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 
 	kubectl_cmd="kubectl -n ${NAMESPACE}"
 
@@ -80,12 +112,17 @@ function datasource_tests() {
 	echo "******************* Executing test suite ${FUNCNAME} ****************"
 	echo ""
 
+<<<<<<< HEAD
   suffix=1
 	for scenario in "${datasource_scenario_order[@]}"; do
+=======
+	for scenario in "${!datasource_scenarios[@]}"; do
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 		echo ""
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		echo " Running datasource scenario: ${scenario}"
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+<<<<<<< HEAD
 		run_datasource_scenario "$scenario" "$suffix"
 
 		if [ "${TESTS_FAILED}" -ne "0" ]; then
@@ -97,6 +134,16 @@ function datasource_tests() {
 	done
 
 	TESTS_FAILED=${TOTAL_TESTS_FAILED}
+=======
+
+		run_datasource_scenario "$scenario"
+
+		if [ "${TESTS_FAILED}" -ne "0" ]; then
+			FAILED_CASES+=("${scenario}")
+		fi
+	done
+
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	TESTS=$(($TESTS_PASSED + $TESTS_FAILED))
 	TOTAL_TESTS_FAILED=${TESTS_FAILED}
 	TOTAL_TESTS_PASSED=${TESTS_PASSED}
@@ -109,16 +156,24 @@ function datasource_tests() {
 	end_time=$(get_date)
 	elapsed_time=$(time_diff "${start_time}" "${end_time}")
 
+<<<<<<< HEAD
 	# Remove the duplicates
 	FAILED_CASES=( $(printf '%s\n' "${FAILED_CASES[@]}" | uniq ) )
 
 	# print the testsuite summary
+=======
+	FAILED_CASES=( $(printf '%s\n' "${FAILED_CASES[@]}" | uniq ) )
+
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	testsuitesummary ${FUNCNAME} "${elapsed_time}" ${FAILED_CASES}
 }
 
 run_datasource_scenario() {
 	local scenario=$1
+<<<<<<< HEAD
 	suffix=$2
+=======
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	POD_LOG="${TEST_SUITE_DIR}/${scenario}-pod.log"
 
 	read PROM_SERVICE THANOS_SERVICE <<< "${datasource_scenarios[$scenario]}"
@@ -127,6 +182,7 @@ run_datasource_scenario() {
 	echo "  Prometheus serviceName = ${PROM_SERVICE}"
 	echo "  Thanos serviceName     = ${THANOS_SERVICE}"
 
+<<<<<<< HEAD
 	update_yaml_with_datasources "${PROM_SERVICE}" "${THANOS_SERVICE}" "${suffix}"
 
 	$kubectl_cmd apply -f "$YAML_FILE" > /dev/null
@@ -157,10 +213,24 @@ run_datasource_scenario() {
         ((TESTS_PASSED++))
       fi
     fi
+=======
+	update_yaml_with_datasources "${PROM_SERVICE}" "${THANOS_SERVICE}"
+
+	$kubectl_cmd apply -f "$YAML_FILE" > /dev/null
+
+	POD_NAME=$($kubectl_cmd get pods | grep kruize | grep -v -E 'db|ui' | awk 'NR==1{print $1}')
+	[ -n "$POD_NAME" ] && $kubectl_cmd delete pod "$POD_NAME"
+
+	if $kubectl_cmd wait --for=condition=Ready pod -l app=$APP_DEPLOYMENT --timeout=120s > /dev/null 2>&1; then
+		echo "Kruize Pod is Ready"
+		POD_NAME=$($kubectl_cmd get pods | grep kruize | grep -v -E 'db|ui' | awk 'NR==1{print $1}')
+		$kubectl_cmd logs "$POD_NAME" > "$POD_LOG" 2>&1
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 	else
 		echo "Kruize Pod failed to come up"
 	fi
 
+<<<<<<< HEAD
 	restore_yaml
 }
 
@@ -214,9 +284,37 @@ update_yaml_with_datasources() {
 		}
 	}' "${YAML_FILE}.ds.bak"
   echo "Updated image in YAML to $AUTOTUNE_IMAGE"
+=======
+  if grep -i "No datasources could be added or are serviceable" "$POD_LOG"; then
+    echo "Expected failure detected"
+    ((TESTS_PASSED++))
+  else
+    echo "Expected failure NOT detected"
+    ((TESTS_FAILED++))
+  fi
+
+	restore_yaml
+}
+
+update_yaml_with_datasources() {
+	local prom_service=$1
+	local thanos_service=$2
+
+	sed -i.ds.bak '
+	/"name": *"prometheus-1"/,/}/{
+		s/"serviceName": *"[^"]*"/"serviceName": "'"$prom_service"'"/
+	}
+	/"name": *"thanos-1"/,/}/{
+		s/"serviceName": *"[^"]*"/"serviceName": "'"$thanos_service"'"/
+	}
+	' "$YAML_FILE"
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 }
 
 restore_yaml() {
 	mv "${YAML_FILE}".ds.bak "$YAML_FILE"
+<<<<<<< HEAD
 	mv "${YAML_FILE}".ds.bak.image.bak "$YAML_FILE"
+=======
+>>>>>>> 3b804615 (add startup tests for datasource validations)
 }
